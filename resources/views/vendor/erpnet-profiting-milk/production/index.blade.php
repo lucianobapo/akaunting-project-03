6d9a3ccf52381a4886b2bc39f642587ebfcf0e71
Luciano Porto <luciano.bapo@gmail.com>
2020-02-11 20:39:04 -0300

    cmt

--- resources/views/vendor/erpnet-profiting-milk/production/index.blade.php
+++ resources/views/vendor/erpnet-profiting-milk/production/index.blade.php
@@ -1,14 +1,32 @@
 @extends('layouts.admin')
 
-@section('title', trans_choice('general.payments', 2))
+@section('title', trans_choice('erpnet-profiting-milk::general.title', 2))
 
-
+@section('new_button')
+    @permission('create-productions')
+    	<span class="new-button">
+    		<a href="{{ route('production.create') }}" class="btn btn-success btn-sm">
+    			<span class="fa fa-plus"></span> &nbsp;{{ trans('general.add_new') }}
+    		</a>
+    	</span>
+    	<span>
+    		<a href="{{ route('production.import') }}" class="btn btn-default btn-sm">
+    			<span class="fa fa-download"></span> &nbsp;{{ trans('import.import') }}
+    		</a>
+    	</span>
+    @endpermission
+    <span>
+    	<a href="{{ route('production.export', request()->input()) }}" class="btn btn-default btn-sm">
+    		<span class="fa fa-upload"></span> &nbsp;{{ trans('general.export') }}
+    	</a>
+    </span>
+@endsection
 
 @section('content')
 <!-- Default box -->
 <div class="box box-success">
     <div class="box-header with-border">
-        {!! Form::open(['url' => 'expenses/payments', 'role' => 'form', 'method' => 'GET']) !!}
+        {!! Form::open(['route' => 'production.index', 'role' => 'form', 'method' => 'GET']) !!}
         <div id="items" class="pull-left box-filter">
             <span class="title-filter hidden-xs">{{ trans('general.search') }}:</span>
             {!! Form::text('search', request('search'), ['class' => 'form-control input-filter input-sm', 'placeholder' => trans('general.search_placeholder')]) !!}
@@ -28,16 +46,15 @@
             <table class="table table-striped table-hover" id="tbl-payments">
                 <thead>
                     <tr>
-                        <th class="col-md-2">@sortablelink('paid_at', trans('general.date'))</th>
-                        <th class="col-md-2 text-right amount-space">@sortablelink('amount', trans('general.amount'))</th>
+                        <th class="col-md-2">@sortablelink('posted_at', trans('erpnet-profiting-milk::table.posted_at'))</th>
+                        <th class="col-md-2 text-right amount-space">@sortablelink('quantity', trans('erpnet-profiting-milk::table.quantity'))</th>
                         <th class="col-md-3 hidden-xs">@sortablelink('vendor.name', trans_choice('general.vendors', 1))</th>
                         <th class="col-md-2 hidden-xs">@sortablelink('category.name', trans_choice('general.categories', 1))</th>
-                        <th class="col-md-2 hidden-xs">@sortablelink('account.name', trans_choice('general.accounts', 1))</th>
                         <th class="col-md-1 text-center">{{ trans('general.actions') }}</th>
                     </tr>
                 </thead>
                 <tbody>
-              
+              		
                 </tbody>
             </table>
         </div>
@@ -45,7 +62,11 @@
     <!-- /.box-body -->
 
     <div class="box-footer">
-        
+        @include('partials.admin.pagination', [
+        	'items' => $productions, 
+        	'type' => 'productions',
+        	'trans' => 'erpnet-profiting-milk::general.title'
+        ])
     </div>
     <!-- /.box-footer -->
 </div>
